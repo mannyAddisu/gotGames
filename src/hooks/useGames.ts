499911606f806d8c6dd132be8729263ff0798ac1
mannyAddisu <codesbycodex@gmail.com>
2024-07-12 15:24:22 +0300

    creating a custom hook

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+interface Game {
+  id: number;
+  name: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort(); //clean up function to handle the cancelled requests
+  }, []);
+  return { games, error };
+};
+export default useGames;
