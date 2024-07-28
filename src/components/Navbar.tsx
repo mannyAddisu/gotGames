52f522776fb3abc7a236f275bdc7bfe5d7e02e88
mannyAddisu <codesbycodex@gmail.com>
2024-07-28 19:09:02 +0300

    routing fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Asset.png";
 import ThemeSwitch from "./ThemeSwitch";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <HStack justifyContent={"space-between"} padding={5}>
-      <Image src={logo} boxSize="60px" />
+      <Link to={"/"}>
+        <Image src={logo} boxSize="60px" objectFit="contain" />
+      </Link>
       <SearchBar />
       <ThemeSwitch />
     </HStack>
