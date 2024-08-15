481981c6f495928dbc3e049cbcfcb223b00bd819
mannyAddisu <codesbycodex@gmail.com>
2024-08-15 09:36:48 +0300

    vercel analytics

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Box padding={4}>
         <Outlet />
+        <Analytics />
       </Box>
     </>
   );
